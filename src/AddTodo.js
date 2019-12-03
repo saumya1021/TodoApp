c10f2a0cadda8048220360e3c399e50838cf5411
ongraph <dev.reactjs@gmail.com>
2019-12-03 18:08:15 +0530

    first commit

---
+++ src/AddTodo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class AddTodo extends Component {
+    state = {
+        content: ''
+    }
+    handleChange = (e) =>{
+        this.setState({
+            content: e.target.value
+        })
+    }
+    handleSubmit= (e) =>{
+        e.preventDefault()
+        this.props.addTodo(this.state)
+    }
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label> Add TODO</label>
+                    <input type='text'  onChange={(e)=>{this.handleChange(e)}}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddTodo
